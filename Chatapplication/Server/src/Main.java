0d4e1576a88e6207fe7dbac9c78ade91480e083d
SaranVairaperumal <83242355+Saran-coder90@users.noreply.github.com>
2024-04-20 11:22:05 +0530

    Delete Chatapplication directory

--- Chatapplication/Server/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Server ob=new Server(5999);
-        ob.chat();
-       // Fileserver obj=new Fileserver();
-    }
-}
--- Chatapplication/client/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Client soc=new Client("localhost",5999);
-        soc.chat();
-    }
-}
